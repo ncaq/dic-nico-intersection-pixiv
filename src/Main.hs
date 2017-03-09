5af93e41868e9f9318011c176d6536a890a6cef5
ncaq <ncaq@ncaq.net>
2017-03-09 20:26:17 +0900

    added: main

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,26 @@
 module Main where
 
 import           Codec.Archive.Zip
-import qualified Data.ByteString.Lazy.Char8 as L8
+import           Control.Monad
+import qualified Data.ByteString.Char8      as B
+import qualified Data.ByteString.Lazy.Char8 as BL8
+import           Data.Monoid
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as T
 import           Data.Text.ICU
-import           Data.Text.ICU.Convert
+import qualified Data.Text.IO               as T
 import           Network.HTTP.Simple
+import           Network.HTTP.Types
 
 main :: IO ()
 main = do
     Archive { zEntries = [ entryAtok@Entry {eRelativePath = "nicoime_atok.txt"}, _ ] } <-
         toArchive . getResponseBody <$> httpLBS "http://tkido.com/data/nicoime.zip"
-    conv <- open "Shift-JIS" Nothing
-    let nicoimeAtok = toUnicode conv $ L8.toStrict $ fromEntry entryAtok
+    l <- filterM (\(_, w) -> (\r -> getResponseStatusCode r == 200) <$>
+                     httpNoBody
+                     (parseRequest_
+                      (B.unpack ("http://dic.pixiv.net/a/" <> urlEncode False (T.encodeUtf8 w))))) .
+         map (\[y, w, _] -> (normalize NFKC y, normalize NFKC w)) .
+         map (T.split ('\t' ==)) . drop 8 . T.lines . T.decodeUtf16LE . BL8.toStrict $
+         fromEntry entryAtok
+    mapM_ (\(y, w) -> T.putStrLn $ y <> "\t" <> w <> "\t" <> "固有一般") l
