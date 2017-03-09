9067cf8e90178d8a6085f1d9c4e3f1350bdf7a1a
ncaq <ncaq@ncaq.net>
2017-03-09 12:19:42 +0900

    added: download nicoimeAtok

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+import           Codec.Archive.Zip
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Data.Text.ICU
+import           Data.Text.ICU.Convert
+import           Network.HTTP.Simple
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    Archive { zEntries = [ entryAtok@Entry {eRelativePath = "nicoime_atok.txt"}, _ ] } <-
+        toArchive . getResponseBody <$> httpLBS "http://tkido.com/data/nicoime.zip"
+    conv <- open "Shift-JIS" Nothing
+    let nicoimeAtok = toUnicode conv $ L8.toStrict $ fromEntry entryAtok
